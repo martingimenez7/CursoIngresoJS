0e85baa517b0cc8a95b078976739735f32945fce
martingimenez7 <martingimenez7@hotmail.com>
2019-02-07 00:17:39 -0300

    Update uno.js

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,22 @@
 
 function mostrar()
-{
+{   
+   var ancho;
+   var largo;
+   var calculoancho;
+   var calculoancho;
+   var perimetro;
 
+   ancho = prompt("ingresar el ancho de un rectangulo");    
+    ancho = parseInt(ancho);
+
+    largo = prompt("ingrese el largo de un rectangulo");
+     largo = parseInt(largo); 
+
+    calculoancho = ancho * 2;
+    calculolargo = largo * 2;
+
+    perimetro = calculoancho + calculolargo;
+  
+   alert("el perimetro del rectangulo es de: " + perimetro);
 }
