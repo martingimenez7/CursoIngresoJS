1392cc67a467ea5e673d58d551e5669d1aefc6d7
martingimenez7 <martingimenez7@hotmail.com>
2019-02-08 22:10:39 -0300

    Update jsFerreteFacturacion.js

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -6,12 +6,87 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 function Sumar () 
 {
 	
+   var preciouno;
+   var preciodos; 
+   var preciotres;
+   var suma;
+   var resultado;
+
+  preciouno = PrecioUno.value;
+  preciouno = parseInt(preciouno);
+
+  preciodos = PrecioDos.value;
+  preciodos = parseInt(preciodos);
+
+  preciotres = PrecioTres.value;
+  preciotres = parseInt(preciotres);
+
+  suma = preciouno + preciodos + preciotres;
+
+  resultado = suma
+
+  alert("la suma es de: " + resultado);
+  
+  //sumar en amarillo en el html
+
 }
 function Promedio () 
 {
 	
+   var preciouno;
+   var preciodos; 
+   var preciotres;
+   var promedio;
+   var suma;
+   var resultado;
+
+  preciouno = PrecioUno.value;
+  preciouno = parseInt(preciouno);
+
+  preciodos = PrecioDos.value;
+  preciodos = parseInt(preciodos);
+
+  preciotres = PrecioTres.value;
+  preciotres = parseInt(preciotres);
+
+  suma = preciouno + preciodos + preciotres;
+  
+  promedio = suma / 3;
+
+  resultado = promedio;
+
+  alert("el promedio es de: " + resultado);
+
 }
 function PrecioFinal () 
 {
 	
+   var preciouno;
+   var preciodos; 
+   var preciotres;
+   var iva;
+   var suma;
+   var resultado;
+   var sumaiva;
+
+  preciouno = PrecioUno.value;
+  preciouno = parseInt(preciouno);
+
+  preciodos = PrecioDos.value;
+  preciodos = parseInt(preciodos);
+
+  preciotres = PrecioTres.value;
+  preciotres = parseInt(preciotres);
+
+  suma = preciouno + preciodos + preciotres;
+  
+  iva = suma * 21 / 100;
+
+  sumaiva = suma + iva;
+
+  resultado = sumaiva;
+
+  alert("el promedio es de: " + sumaiva);
+
+
 }
