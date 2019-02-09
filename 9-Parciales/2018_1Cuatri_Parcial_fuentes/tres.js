10cc411e8c5a26d5abb66eda1c92a9c2e276413c
martingimenez7 <martingimenez7@hotmail.com>
2019-02-08 22:10:43 -0300

    Update tres.js

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,19 @@
 function mostrar()
 {
 
+  var precio;
+  var descuento;
+  var resultado;
+
+  precio = prompt("por favor, ingrese el precio");
+  precio = parseInt(precio);
+
+  descuento = prompt("por favor, ingrese el descuento");
+  descuento = parseInt(descuento);
+
+  descuento = precio * descuento / 100;
+
+  resultado = precio - descuento;
+  
+  elPrecioFinal.value = resultado;
 }
