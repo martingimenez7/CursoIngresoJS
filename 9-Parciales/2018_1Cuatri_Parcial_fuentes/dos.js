a3d6b9997aa66fac411d82ed71c5b8bf52bfc4b9
martingimenez7 <martingimenez7@hotmail.com>
2019-02-08 22:10:41 -0300

    Update dos.js

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,10 @@
 function mostrar()
 {
-  
+  var nombre;
+  var localidad;
+
+  nombre = elNombre.value;
+  localidad = laLocalidad.value;
+
+  alert("usted es " + nombre + " y vive en la localidad de " + localidad);
 }
