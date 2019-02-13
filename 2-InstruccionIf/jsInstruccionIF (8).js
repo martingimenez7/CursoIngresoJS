715cd318db7785768ef6893cb959827235f7f823
martingimenez7 <martingimenez7@hotmail.com>
2019-02-13 00:09:26 -0300

    Update jsInstruccionIF (8).js

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -2,7 +2,17 @@ function mostrar()
 {
 //tomo la edad  
 
-	
+  var laedad;
+  laedad = edad.value;
+  var estadoc;
+  estadoc = estadoCivil.value;
 
+  if(laedad>17)
+  {
+  	if(estadoc)
+  	{
+  		alert("usted es soltero y no es menor");
+  	}
+  }
 
 }//FIN DE LA FUNCIÓN
