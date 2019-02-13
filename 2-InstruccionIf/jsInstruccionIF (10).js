5fd9350c20ed993610b4df67d2641b05329d0064
martingimenez7 <martingimenez7@hotmail.com>
2019-02-13 00:09:22 -0300

    Update jsInstruccionIF (10).js

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,27 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+
+    var random;
+   
+   random = prompt("ingresar la nota");
+   random = parseInt(random);
+
+    
+    if (random>8&&random<11)
+     {
+         alert("EXELENTE");
+     }
+    
+   if (random>4&&random<9)
+     {
+         alert("aprobo");
+     }
+
+   if (random<4)
+     {    	
+         alert("vamos, la proxima se puede");
+     }
+    
 
 }//FIN DE LA FUNCIÓN
