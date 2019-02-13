d2685cd4ea726836a60afbee1cb676200832e32c
martingimenez7 <martingimenez7@hotmail.com>
2019-02-13 15:09:24 -0300

    Update uno.js

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,6 +1,27 @@
 
 function mostrar()
 {   
+  var nombreuno;
+  var nombredos;
+  var edaduno;
+  var edaddos;
+  var suma;
+
+   nombreuno =  prompt ( " ingrese su nombre " );
+   nombredos =  prompt ( " ingrese su nombre " );
+   edaduno =  pronta ( " ingrese su edad " );
+   edaduno =  parseInt (edaduno);
+   edaddos =  prompt ( " ingrese su edad " );
+   edaddos =  parseInt (edaddos);
+
+   suma = edaduno + edaddos;
+
+   alerta ( " ustedes son " + nombreuno + " y " + nombredos + " , sus edades son " + edaduno + " y " + edaddos + " , y la suma de sus edades es " + suma);
+}
+
+
+
+/*{   
    var ancho;
    var largo;
    var calculoancho;
