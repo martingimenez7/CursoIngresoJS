e033e0090eee0afaa1ef6ff428987036475af1d8
martingimenez7 <47359197+martingimenez7@users.noreply.github.com>
2019-02-13 12:03:17 -0300

    Add files via upload

---
+++ jsInstruccionIF (10).js
@@ -0,0 +1,26 @@
+function mostrar()
+{
+	//Genero el número RANDOM entre 1 y 10 
+    
+    var mensaje;
+    var numerorandom;
+    var numerorandom =Math.floor((Math.random() * 10) + 1); 
+   
+    if (numerorandom>8&&numerorandom<11)
+     {
+         mensaje="EXELENTE";
+     }
+    
+   if (numerorandom>4&&numerorandom<9)
+     {
+         mensaje= "aprobo";
+     }
+
+   if (numerorandom<4)
+     {    	
+        mensaje="vamos, la proxima se puede";
+     }
+   
+    alert(mensaje);
+
+}//FIN DE LA FUNCIÓN
---
+++ jsInstruccionIF (9).js
@@ -0,0 +1,11 @@
+function mostrar()
+{
+	//Genero el número RANDOM entre 1 y 10 
+   
+    var numerorandom;
+    var numerorandom =Math.floor((Math.random() * 10) + 1); 
+   
+   alert(numerorandom);
+   
+   
+}//FIN DE LA FUNCIÓN
