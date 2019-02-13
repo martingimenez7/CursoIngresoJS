36af7bf7fdca5022c559ec860e7b11c499e90c99
martingimenez7 <martingimenez7@hotmail.com>
2019-02-13 00:09:28 -0300

    Update cuatro.js

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,45 @@
 function mostrar()
 {
+  var numerouno;
+  var numerodos;
+  var resultado;
+  var suma;
 
+  numerouno = prompt("ingrese un numero");
+  numerouno = parseInt(numerouno);
+  numerodos = prompt("ingrese otro numero");
+  numerodos = parseInt(numerodos);
+  resultado = numerouno+numerodos;
+
+  if (numerouno&&numerodos);
+  {
+  	if(numerouno==numerodos)
+  	{
+  	    alert(numerouno + numerodos);
+  	}
+  	
+  }
+  
+  if(numerouno||numerodos)
+  {
+  	if(numerouno>numerodos)
+  	{
+  		alert(numerouno - numerodos);
+  	}
+  	if (numerouno<numerodos)
+  	{
+  		alert(numerouno+numerodos);
+  	}
+  }
+
+  if ()
+  {
+  	if(resultado>10)
+  	{
+  		alert("el resultado es: "+resultado+" y supero el 10");
+  	}
+  }
+
+  
 }
+
