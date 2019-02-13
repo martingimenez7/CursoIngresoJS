9b8f04fbdbb6f21f7dd7c7e0c3761e6efe7905cb
martingimenez7 <martingimenez7@hotmail.com>
2019-02-13 15:09:21 -0300

    Update cuatro.js

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,5 +1,51 @@
 function mostrar()
 {
+  var cantidad;
+  var descuentouno;
+  var descuentodos;
+  var metododepago;
+  var recargo;
+  var precioproducto;
+  var calculouno;
+  var calculodos;
+  var preciofinal;
+
+  cantidad =  pronta ( " ingrese la cantidad de productos comprados " );
+  precioproducto =  prompt ( " ingrese el precio de / los productos " );
+  metododepago =  prompt ( " paga con targeta o efectivo? " );
+
+
+  calculouno =  parseInt (calculouno);  
+  calculodos =  parseInt (calculodos);  
+  recargo =  parseInt (recargo);
+  preciodelproducto = parseInt (precioproducto);
+
+  si (cantidad > 2 )
+  {
+    calculouno = precioproducto * 10 / 100 ;
+    preciofinal = precioproducto + calculouno;
+  }
+  si (precioproducto > 1999 )
+  {
+    calculodos = precioproducto * 15 / 100 ;
+    calculouno = precioproducto * 10 / 100 ;
+    preciofinal = precioproducto - calculodos - calculouno;
+  }
+
+  if (metododepago == " targeta " )
+  {
+    recargo = precioproducto * 10 / 100 ;
+    preciofinal = preciofinal + recargo;
+  }
+  if (metododepago == " efectivo " )
+  {
+
+  }
+  alert( " el monto final es de "  + preciofinal);
+}
+
+
+/*{
   var numerouno;
   var numerodos;
   var resultado;
