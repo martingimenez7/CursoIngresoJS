4593b916d61aafeb9e6b93530a6779a6a15231b3
martingimenez7 <martingimenez7@hotmail.com>
2019-02-13 15:09:14 -0300

    Update dos.js

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,5 +1,26 @@
 function mostrar()
 {
+  var precioefectivo;
+  var preciotargeta;
+  var recargo;
+  var preciofinal;
+
+  precioefectivo =  prompt ( " ingrese el precio del producto " );
+
+  precioefectivo =  parseInt (precioefectivo);
+
+  preciotargeta =  parseInt (preciotargeta);
+
+  recargo = precioefectivo *  10  / 100 ;
+
+  preciotargeta = precioefectivo + recargo;
+
+  preciofinal = preciotargeta;
+
+    alerta ( " el precio del producto en efectivo es " + precioefectivo + " , con tarjeta tiene un recargo del 10%, que seria " + recargo + " , costandole final " + preciofinal);
+}
+
+/*{
   var nombre;
   var localidad;
 
