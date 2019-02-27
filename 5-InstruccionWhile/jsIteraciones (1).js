63333b06edd2c953e0ddf79626b5539489f97d7a
martingimenez7 <martingimenez7@hotmail.com>
2019-02-26 22:22:20 -0300

    9

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,12 @@
 function mostrar()
-{
-	alert('iteración while');
+{ 
+	var contador=0;
 
+   while(contador<10)
+    {
+ 
+     contador=contador+1;
+     console.log(contador);	
+    }
 
 }//FIN DE LA FUNCIÓN
