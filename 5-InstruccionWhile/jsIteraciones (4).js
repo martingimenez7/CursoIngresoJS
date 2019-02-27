2dca5534dcfc72cc58f8363e7b22893c9f6c830e
martingimenez7 <martingimenez7@hotmail.com>
2019-02-26 22:22:26 -0300

    Update jsIteraciones (4).js

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -2,6 +2,11 @@ function mostrar()
 {
 
 	var numero = prompt("ingrese un número entre 0 y 10.");
-
+    
+    while(numero<10)
+    {
+    	Numero.value = numero;
+    	break
+    }
 
 }//FIN DE LA FUNCIÓN
