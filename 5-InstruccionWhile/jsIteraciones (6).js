13ee3ad90cd5756633410f68fc6b3b4b3127af81
martingimenez7 <martingimenez7@hotmail.com>
2019-02-26 22:22:37 -0300

    Update jsIteraciones (6).js

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,8 +3,18 @@ function mostrar()
 
 	var contador=0;
 	var acumulador=0;
+	var numero =0;
+
+    numero = prompt("ingrese un numero");
+    numero = parseInt(numero);
 
 
+    while(contador<4)
+    {
+        numero = prompt("ingrese otro numero");
+        contador++;
+        acumulador++;
+    }
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/5;
